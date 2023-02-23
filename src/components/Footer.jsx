4179bcf9a4a9f4ff8fbecfcbc64da310b57f9459
Isaac Lal <isaaclal124@gmail.com>
2023-02-23 11:24:51 -0500

    changed footer design and icon again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,17 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='flex flex-col'>
-      <footer
+      <div
         className='
-        translate-y-28	
+        sticky
              text-3xl text-gray-400 text-center
              font-bold
              border-t-4 border-gray-500
-             inset-x-0
              bottom-0
              p-4'
       >
         Created by <span className='text-[#00bfff]'>Isaac Lal</span>, © 2023
-      </footer>
+      </div>
     </div>
   );
 };
